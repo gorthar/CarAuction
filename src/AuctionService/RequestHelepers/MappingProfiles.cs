14ca4c242f16606ff11a5bd9b1dfcd93d87c3b63
gorthar <dkahraman08@gmail.com>
2023-08-10 13:48:51 -0700

    First service(CRUD operation of auctions) created.

---
+++ src/AuctionService/RequestHelepers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDtos>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDtos>();
+        CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
